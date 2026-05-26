3d71cc276341a1f0f69b8c090e8018e33b207829
restgenmgr <restarantbarmanager@gmail.com>
2026-05-26 10:05:02 +0530

    Fix PayPortal: Clean React imports and fix TypeScript errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Blog from './pages/Blog';
 import HotelSOP from "./pages/HotelSOP";
